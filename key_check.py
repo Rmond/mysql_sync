c0b61d9eaa680e8b4f70c67077b0ec5bcbebdd1a
710324581@qq.com <kongtiao97>
2020-01-14 13:48:57 +0800

    fix unique index no name bug

--- key_check.py
+++ key_check.py
@@ -66,9 +66,9 @@ def getkey_sql(tab,key_property,key_dict,mode):
         if(key_property == 0 and key_name.lower() == 'primary'): #主键
             sql = "alter table " + str(tab) + " add primary key (" + str(key_column) +");"
         elif(key_property==0 and key_name.lower()!='primary'):#唯一索引                sql = "alter table " + str(tab) + " add " + str(key_name) + "("+key_column+");"
-            sql = "alter table " + str(tab) + " add unique index " + str(key_name) + "("+key_column+");"
+            sql = "alter table " + str(tab) + " add unique index `" + str(key_name) + "`("+key_column+");"
         else:#普通索引
-            sql = "alter table " + str(tab) + " add index " + str(key_name) + "(" + key_column+");"
+            sql = "alter table " + str(tab) + " add index `" + str(key_name) + "`(" + key_column+");"
     else:#删除索引
         if(key_name.lower() == 'primary'):
             sql = "alter table " + str(tab) + " drop primary key;"
